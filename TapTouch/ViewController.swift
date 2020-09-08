3c51d5c7aad08388458d718ab5746e4912e1763b
sung <chin_sung@naver.com>
2020-09-08 13:34:11 +0900

    feat tap/touch count

--- TapTouch/ViewController.swift
+++ TapTouch/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var txtmessage: UILabel!
+    @IBOutlet var txtTapcount: UILabel!
+    @IBOutlet var txtTouchCount: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first! as UITouch
+        txtmessage.text = "Touches Began"
+        txtTapcount.text = String(touch.tapCount)
+        txtTouchCount.text = String(touches.count)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first! as UITouch
+        txtmessage.text = "Touches Moved"
+        txtTapcount.text = String(touch.tapCount)
+        txtTouchCount.text = String(touches.count)
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first! as UITouch
+        txtmessage.text = "Touches Ended"
+        txtTapcount.text = String(touch.tapCount)
+        txtTouchCount.text = String(touches.count)
+    }
 
 }
 
